aa92e1d507a3aa27bb628337e1fa083e6d259112
force589 <firasmuhammed05@gmail.com>
2026-08-25 16:49:39 +0530

    Upgrade mobile app to Expo SDK 57

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'jest-expo',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testTimeout: 30000,
   testPathIgnorePatterns: ['/node_modules/', '/.expo/'],
   collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
 };
-
